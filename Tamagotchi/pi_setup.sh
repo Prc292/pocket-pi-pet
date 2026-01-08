9d18febce7f205e595232c5933ad4a09a9dac04f
Brian <price292@icloud.com>
2026-01-07 22:03:29 -0500

    startup scripts

--- Tamagotchi/pi_setup.sh
+++ Tamagotchi/pi_setup.sh
@@ -4,7 +4,7 @@
 set -e
 
 REPO_URL="https://github.com/Prc292/pipet.git"
-
+SERVICE_FILE="/etc/systemd/system/pipet.service"
 
 
 echo "--- STARTING AUTOMATED SETUP for pipet ---"
@@ -17,28 +17,43 @@ sudo apt upgrade -y
 # 2. Install required system libraries and SDL2 runtime/development packages
 echo "2. Installing required system libraries..."
 sudo apt install -y python3 python3-pip python3-venv python3-dev python3-setuptools
-    \ git \ libSDL2-2.0-0 
-    \ libSDL2-dev libSDL2-image-2.0-0 
-    \ libsdl2-image-dev \ libsdl2-mixer-dev 
-    \ libsdl2-ttf-dev \ libportmidi-dev 
-    \ libfreetype6-dev \ libjpeg-dev 
-    \ libbz2-dev \ libpng-dev \ libopenal-dev 
-    \ libudev-dev \ build-essential
+    \ git \ libegl-dev \ libsdl2-2.0-0
 
 # 4. Create Python virtual environment and install dependencies
 echo "4. Creating Python virtual environment..."
-python3 -m venv ~/pipet/venv
+cd /home/brian/pipet
+python3 -m venv venv
 source "~/pipet/venv/bin/activate"
 pip install --upgrade pip
 pip install -r ~/pipet/Tamagotchi/requirements.txt
 
 # 5. Set executable permissions
 echo "5. Setting executable permissions for main.py..."
-cd Tamagotchi
+cd /home/brian/Tamagotchi
 chmod +x main.py
 
+# setup pipet.service systemd service file
+echo "6. Setting up pipet.service systemd service..."
+SERVICE_FILE="/etc/systemd/system/pipet.service"
+sudo bash -c "cat > $SERVICE_FILE" <<EOL
+[Unit]
+Description=Pocket Pi Pet Service
+After=network.target
+[Service]
+Environment="SDL_FBDEV=/dev/fb0"
+Environment="SDL_MOUSEDEV=/dev/input/event0"
+Environment="SDL_MOUSEDRV=TSLIB"
+User=brian
+Group=brian
+WorkingDirectory=/home/brian/pipet
+ExecStart=/home/brian/pipet/venv/bin/python /home/brian/pipet/Tamagotchi/main.py
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOL
+
 echo "--- SETUP COMPLETE ---"
-echo "The game is installed in ~/pipet
+echo "The game is installed in /home/brian/pipet"
 echo ""
 echo "TO RUN THE GAME:"
 echo "  source ~/pipet/venv/bin/activate"
